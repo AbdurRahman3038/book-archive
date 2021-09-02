140f89e1a20079fcafe0d5ca13a24d70dc1e12a8
AbdurRahman3038 <abdurrahman.iubat@gmail.com>
2021-09-02 17:09:03 +0600

    final checking done

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
+
+// function for fetching data with input search value 
+
 const searchBook = () => {
   const searchInput = document.getElementById('search-input');
   const searchText = searchInput.value;
-
+  
+//error message
   if (searchText === '') {
     const bookAmount = document.getElementById('book-amount');
     bookAmount.style.display = "block";
@@ -19,62 +23,50 @@ const searchBook = () => {
 
 }
 
+//--------------------------------
+// function for displaying result 
+//-------------------------------
 
 const displaySearchResult = books => {
   document.getElementById('display-books').innerHTML = '';
 
-if(books.length === 0){
-  const bookAmount = document.getElementById('book-amount');
-  bookAmount.style.display = "block";
-  bookAmount.innerText = "No Result Found!!!";
-}
+//showing error mesage 
 
-  else{
+  if (books.length === 0) {
     const bookAmount = document.getElementById('book-amount');
-  bookAmount.style.display = "block";
-  bookAmount.innerText = `Number of Books found ${books.length} out of 9900`;
+    bookAmount.style.display = "block";
+    bookAmount.innerText = "No Result Found!!!";
+  }
 
+  else {
+    const bookAmount = document.getElementById('book-amount');
+    bookAmount.style.display = "block";
+    bookAmount.innerText = `Number of Books found ${books.length} out of 9900`;
 
+    books.forEach(book => {
 
-  books.forEach(book => {
+      const displayBooks = document.getElementById('display-books');
 
-    // const bookFilter = book.filter(searchBook => searchBook.title === document.getElementById('search-input') )
-    //         console.log(bookFilter);
+      const div = document.createElement('div');
+      div.classList.add('col');
 
-    const displayBooks = document.getElementById('display-books');
+      const imageUrl = `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`;
 
-    const div = document.createElement('div');
-    div.classList.add('col');
-
-    const imageUrl = `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`;
-
-    div.innerHTML = `
+      div.innerHTML = `
         <div class="card h-100">
             <img style=" height: 400px" src="${imageUrl}" class="card-img-top img-fluide" alt="...">
             <div class="card-body">
               <h5 class="card-title">${book.title}</h5>
               <h6 class="card-title">${book.author_name}</h6>
-              <p class="card-text">${book.publisher}</p>
+              <p class="card-text">${book.publisher[0]}</p>
               <p class="card-text">${book.first_publish_year}</p>
               
             </div>
           </div>
         `;
 
-    displayBooks.appendChild(div);
-  });
+      displayBooks.appendChild(div);
+    });
 
   }
 }
-
-// const displayBookAmount = books =>{
-
-//   books.forEach(book =>{
-//     const displayAmount = document.getElementById('book-amount');
-//     const div = document.createElement('div');
-//           div.classList.add('w-25 mx-auto');
-//           div.innerHTML =`
-//           <p>Number of Books Found ${book} </p>
-//           `;
-//   });
-// }
